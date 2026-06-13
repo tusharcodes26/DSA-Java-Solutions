da09aa91236184b14d3fe66ccaae7921b5b97abf
Tushar <tchhabra0739@gmail.com>
2026-06-13 19:31:14 +0530

    DSA-JAVA-SOLUTIONS

--- mergeSort.java
+++ mergeSort.java
@@ -1,70 +1,51 @@
-public class mergeSort{
-    public static void sort(int[] arr, int left, int right){
-        if(left < right){
-            int mid = left + (right - left) / 2;
+public class mergeSort {
+    static class Solution {
+        void merge(int[] arr, int low, int mid, int high) {
+            int[] temp = new int[high - low + 1];
+            int left = low, right = mid + 1, k = 0;
 
-            sort(arr, left, mid);
-            sort(arr, mid + 1, right);
-
-            merge(arr, left, mid, right);
-        }
-    }
-
-    public static void merge(int[] arr, int left, int mid, int right){
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        int[] leftArr = new int[n1];
-        int[] rightArr = new int[n2];
-
-        for(int i = 0; i < n1; i++){
-            leftArr[i] = arr[left + i];
-        }
-
-        for(int j = 0; j < n2; j++){
-            rightArr[j] = arr[mid + 1 + j];
-        }
-
-        int i = 0, j = 0, k = left;
-
-        while(i < n1 && j < n2){
-            if(leftArr[i] <= rightArr[j]){
-                arr[k] = leftArr[i];
-                i++;
+            while (left <= mid && right <= high) {
+                if (arr[left] <= arr[right]) {
+                    temp[k++] = arr[left++];
+                } else {
+                    temp[k++] = arr[right++];
+                }
             }
-            else{
-                arr[k] = rightArr[j];
-                j++;
+
+            while (left <= mid) {
+                temp[k++] = arr[left++];
             }
-            k++;
+
+            while (right <= high) {
+                temp[k++] = arr[right++];
+            }
+
+            for (int i = low; i <= high; i++) {
+                arr[i] = temp[i - low];
+            }
         }
 
-        while(i < n1){
-            arr[k] = leftArr[i];
-            i++;
-            k++;
-        }
-        while(j < n2){
-            arr[k] = rightArr[j];
-            j++;
-            k++;
+        void mergeSort(int[] arr, int low, int high) {
+            if (low >= high) {
+                return;
+            }
+
+            int mid = (low + high) / 2;
+
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {38, 27, 43, 3, 9, 83, 10};
+        int[] arr = {5, 2, 8, 4, 1};
+        Solution sol = new Solution();
 
-        System.out.println("Before Merge Sort");
-        for(int num : arr){
-            System.out.print(num + " ");
-        }
-        System.out.println();
+        sol.mergeSort(arr, 0, arr.length - 1);
 
-        sort(arr, 0, arr.length - 1);
-
-        System.out.println("After Merge Sort");
-        for(int num : arr){
-            System.out.print(num + " ");
+        for (int x : arr) {
+            System.out.print(x + " ");
         }
         System.out.println();
     }
